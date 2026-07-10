e2d04dc4eca6e544760262bfe8a0de7ace58ece7
gunasekhar0602 <gunabandla2000@gmail.com>
2026-07-10 12:46:31 +0530

    Update Module.ts

--- Module.ts
+++ Module.ts
@@ -1,9 +1,10 @@
-1. Named Imports (Most common)
+// 1. Named Imports (Most common)
  
-Used when the module exports multiple things using export.
+// Used when the module exports multiple things using export.
  
-Module.ts
- 
+// another file Module.ts
+
+// exporting that particular method, function or variable
 export const AppName = "MyApp";
  
 export function add(a: number, b: number): number { return a + b;
@@ -11,21 +12,21 @@ export function add(a: number, b: number): number { return a + b;
 }
  
 Usage:
- 
+// importing that particular method, function or variable from Module file
 import { AppName, add } from "./Module";
  
  
  
-2. Aliased Imports
+// 2. Aliased Imports
  
-You can rename imports for clarity or conflict avoidance.
+// You can rename imports for clarity or conflict avoidance.
  
 import { add as addNumbers, AppName as NameOfApp } from "./Module";
 
 
-3. Import Everything (Namespace Import)
+// 3. Import Everything (Namespace Import)
  
-If you want to import the whole module under a single object.
+// If you want to import the whole module under a single object.
  
 import * as Utils from "./Module";
  
